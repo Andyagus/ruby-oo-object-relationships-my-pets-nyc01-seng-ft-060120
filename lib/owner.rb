2d1ad8749ab95b971664e0b2e58ce333775429b9
orlandofat <orlandofat@gmail.com>
2020-06-04 16:04:02 -0400

    update to buy dog

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,7 +30,29 @@ class Owner
         Cat.all.select do |cat|
             cat.owner == self
         end
-    end
+	end
+	def dogs
+		Dog.all.select do |dog|
+			dog.owner == self
+		end
+	end
 
+   def buy_cat(name)
+	   Cat.new(name, self)
+   end
+
+   def buy_dog(name)
+	   Dog.new(name, self)
+   end
+
+   def walk_dogs
+	   [self].each {|dog|dog.mood= "happy"} 
+   end
+
+   def list_pets
+	   @@all.map do |pets|
+		pets.count
+		end
+   end
 end
 
